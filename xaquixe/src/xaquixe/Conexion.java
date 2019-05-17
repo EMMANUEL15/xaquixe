09a7d568927d4880010d1457c32fee840d9ef206
equipo <equipo@10.168.66.58>
2019-05-17 15:11:09 -0600

    TOKENS

---
+++ xaquixe/src/xaquixe/Conexion.java
@@ -0,0 +1,49 @@
+package xaquixe;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Emanuel lopez
+ */
+public class Conexion {
+         private String  host     = "localhost";
+    private String  usuario     = "postgres";
+    private String  clave       = "lopez";
+    private int     puerto      = 5434;
+    private String  servidor    = "";
+    private static Connection conexion  = null;
+    private String baseDatos;
+    
+    public Conexion(String baseDatos){
+        this.baseDatos = baseDatos;
+        ConexionBd();
+    }
+    
+    public void ConexionBd(){
+        this.servidor="jdbc:postgresql://"+host+":"+ puerto+"/"+baseDatos;
+ 
+        //Registrar el driver
+        try {            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("ERROR AL REGISTRAR EL DRIVER " + e);
+            System.exit(0); //parar la ejecución
+        }
+ 
+        //Establecer la conexión con el servidor
+        try {
+            conexion = DriverManager.getConnection(this.servidor,
+                        this.usuario, this.clave);
+        } catch (SQLException e) {
+            System.err.println("ERROR AL CONECTAR CON EL SERVIDOR");
+            System.exit(0); //parar la ejecución
+        }
+        System.out.println("Conectado a "+ baseDatos);
+    }
+ 
+    //Devuelve el objeto Connection que se usará en la clase Controller
+    public Connection getConexion() {
+        return conexion;
+    }
+}
---
+++ xaquixe/src/xaquixe/Xaquixe.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xaquixe;
+/**
+ * @author Emanuel lopez
+ */
+public class Xaquixe {
+    public static void main(String[] args) {
+         System.out.println("Hola Equipo No compila 2.0");
+            //VISTAS
+        
+            //MODELO
+        
+            //CONEXION A BASE DE DATOS
+        
+            //CONEXION DE VISTA MODELO CONTROLADOR
+        
+            //VINCULAR VISTA Y CONTROLAR
+            
+    }
+}
